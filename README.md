a0d24dd2af3d1e4ff4fb679730f9a4dec709fb97
Florian Schumacher <>
2020-12-08 10:56:47 +0100

    2 trained nets + iterate/classify

--- README.md
+++ README.md
@@ -18,3 +18,12 @@ import cv2
 
 https://chromedriver.chromium.org/downloads
 copy to usr/local/bin/
+
+# net train
+make_image_classifier   --image_dir /Users/flo/Desktop/backup_output_biotop/training_patches/   --tfhub_module https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4   --image_size 224   --saved_model_dir my_dir/new_model   --labels_output_file class_labels.txt   --tflite_output_file new_mobile_model.tflite --train_epochs 20
+
+
+make_image_classifier   --image_dir /Users/flo/Desktop/backup_output_biotop/training_patches/   --tfhub_module https://tfhub.dev/google/imagenet/inception_v3/feature_vector/4   --image_size 80   --saved_model_dir my_dir/new_model   --labels_output_file class_labels.txt   --tflite_output_file new_mobile_model.tflite --train_epochs 20
+
+
+python iterate_image_patches.py   --input_mean 0 --input_std 255   --model_file new_mobile_model.tflite --label_file class_labels.txt --image /Users/flo/Proj/sat_hedge_detection/data/_temp_sliding_window/im/32.png
---
+++ src/net/iterate_image_patches.py
@@ -0,0 +1,190 @@
+# go through biotop_dir, show image,ask label
+import cv2
+import os
+from shutil import copyfile
+import numpy as np
+import imutils
+
+# from __future__ import absolute_import
+# from __future__ import division
+# from __future__ import print_function
+
+import argparse
+import time
+
+import numpy as np
+from PIL import Image
+import tensorflow as tf  # TF2
+
+
+def pyramid(image, scale=1.5, minSize=(20, 20)):
+    # yield the original image
+    yield image
+    # keep looping over the pyramid
+    while True:
+        # compute the new dimensions of the image and resize it
+        w = int(image.shape[1] / scale)
+        if w==0:
+            w=1
+            print("Excepten w:0->1")
+            break
+        image = imutils.resize(image, width=w)
+        # if the resized image does not meet the supplied minimum
+        # size, then stop constructing the pyramid
+        if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
+            break
+        # yield the next image in the pyramid
+        yield image
+
+
+def sliding_window(image, stepSize, windowSize):
+    # slide a window across the image
+    for y in range(0, image.shape[0], stepSize):
+        for x in range(0, image.shape[1], stepSize):
+            # yield the current window
+            yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
+
+
+def load_labels(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def iterate_patches(files, bio_folder, args, height, width):
+
+    borderfile = [x for x in files if "B" in x and "cropped" not in x][0]
+    rawfile = [x for x in files if "M" not in x and "cropped" not in x if "B" not in x][0]
+    cropfile = [x for x in files if "crop" in x][0]
+    # Open image with red border line and make into Numpy array
+    # im = Image.open('../data/output_biotop_dir/' + bio_folder + '/' + borderfile).convert('RGB')
+    image = cv2.imread('../../data/output_biotop_dir/' + bio_folder + '/' + borderfile)
+    image_raw = cv2.imread('../../data/output_biotop_dir/' + bio_folder + '/' + rawfile)
+    image_mask = cv2.imread('../../data/output_biotop_dir/' + bio_folder + '/' + cropfile, cv2.IMREAD_UNCHANGED)
+    write_count = 1
+    c_bio = c_nbio = 0
+    # SLIDING WINDOW
+    (winW, winH) = (16, 16)
+    # loop over the image pyramid
+    for resized in pyramid(image_mask, scale=1000):
+        # loop over the sliding window for each layer of the pyramid
+        for (x, y, window) in sliding_window(resized, stepSize=winW, windowSize=(winW, winH)):
+            mod_im = resized
+            # if the window does not meet our desired window size, ignore it
+            if window.shape[0] != winH or window.shape[1] != winW:
+                continue
+            # THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
+            # MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
+            # WINDOW
+            # since we do not have a classifier, we'll just draw the window
+            clone = resized.copy()
+
+            #cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+            #cv2.imshow("window", clone)
+
+            # if key == ord('1'):
+            #    continue
+
+            # Disregard windows outside of biotop/border cases
+            threshold_black = 100
+            gray_version = cv2.cvtColor(window, cv2.COLOR_BGR2GRAY)
+            if (cv2.countNonZero(gray_version) > threshold_black):
+            #    cv2.imwrite("../data/_temp_sliding_window/"+str(write_count)+".png",window)
+            #    write_count = write_count +1
+            # img = Image.open(args.image)
+                img = Image.fromarray(window).convert('RGB')
+                img = img.resize([width, height])
+
+                # add N dim
+                input_data = np.expand_dims(img, axis=0)
+                if floating_model:
+                    input_data = (np.float32(input_data) - args.input_mean) / args.input_std
+
+                interpreter.set_tensor(input_details[0]['index'], input_data)
+
+                interpreter.invoke()
+
+                output_data = interpreter.get_tensor(output_details[0]['index'])
+                results = np.squeeze(output_data)
+
+                top_k = results.argsort()[-5:][::-1]
+                labels = load_labels(args.label_file)
+
+
+                if top_k[1] == 1:
+                    c_bio = c_bio+1
+                    mod_im = cv2.rectangle(mod_im, (x, y), (x + winW, y + winH), (0, 255, 0), 1)
+                else:
+                    c_nbio = c_nbio+1
+                    mod_im = cv2.rectangle(mod_im, (x, y), (x + winW, y + winH), (0, 0, 255), 1)
+
+                # for i in top_k:
+                #     if floating_model:
+                #         print('{:08.6f}: {}'.format(float(results[i]), labels[i]))
+                #     else:
+                #         print('{:08.6f}: {}'.format(float(results[i] / 255.0), labels[i]))
+        percent = (c_bio*100)/(c_bio+c_nbio)
+        #print(c_bio, "/", c_bio+c_nbio, " bio%: ", percent)
+        print('{:02.2f} % Biotop // Image Patches: {}'.format(float(percent), c_bio+c_nbio))
+        cv2.imshow("window", mod_im)
+        key = cv2.waitKey(0)
+        print("------")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-i',
+        '--image',
+        default='/tmp/grace_hopper.bmp',
+        help='image to be classified')
+    parser.add_argument(
+        '-m',
+        '--model_file',
+        default='/tmp/mobilenet_v1_1.0_224_quant.tflite',
+        help='.tflite model to be executed')
+    parser.add_argument(
+        '-l',
+        '--label_file',
+        default='/tmp/labels.txt',
+        help='name of file containing labels')
+    parser.add_argument(
+        '--input_mean',
+        default=127.5, type=float,
+        help='input_mean')
+    parser.add_argument(
+        '--input_std',
+        default=127.5, type=float,
+        help='input standard deviation')
+    parser.add_argument(
+        '--num_threads', default=None, type=int, help='number of threads')
+    args = parser.parse_args()
+
+    interpreter = tf.lite.Interpreter(
+        model_path=args.model_file, num_threads=args.num_threads)
+    interpreter.allocate_tensors()
+
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    # check the type of the input tensor
+    floating_model = input_details[0]['dtype'] == np.float32
+
+    # NxHxWxC, H:1, W:2
+    height = input_details[0]['shape'][1]
+    width = input_details[0]['shape'][2]
+
+    PATH = '../../data/output_biotop_dir/'
+    folders = os.listdir(PATH)
+    i = 0
+    # Remove ".DS_Store"
+    folders[:] = [x for x in folders if ".DS_Store" not in x]
+    for folder in folders:
+        i = i + 1
+        files = os.listdir(PATH + folder)
+        files[:] = [x for x in files if ".DS_Store" not in x]
+        if (len(files) != 1 and len(files) != 0):
+            print('crop: ' + folder, i, "/", len(folders))
+            iterate_patches(files, folder, args, height, width)
+        #break
+        # crop_v2(files,folder)
+
+# txt = input("Type something to test this out: ")
